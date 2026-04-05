78831429a9274b21ecf9c33d93e9490548a17b14
Saedul.Hussain <saedulhussian99x@gmail.com>
2026-04-05 16:47:39 +0100

    +Vehicle Collision on Bridge Bug Fix +Track 3 Visual Enhancements

--- Assets/Scripts/Track/BridgeHandler.cs
+++ Assets/Scripts/Track/BridgeHandler.cs
@@ -1,4 +1,3 @@
-using System;
 using UnityEngine;
 using Utilities;
 
@@ -15,6 +14,13 @@ public class BridgeHandler : MonoBehaviour
     [field: SerializeField] public BoxCollider2D UnderPassOutCollider { get; set; }
     [field: SerializeField] public BoxCollider2D OverPassInCollider { get; set; }
     [field: SerializeField] public BoxCollider2D OverPassOutCollider { get; set; }
+
+    private CapsuleCollider2D _playerOneCollider;
+    private CapsuleCollider2D _playerTwoCollider;
+    private bool _isPlayerOneUnderPass = false;
+    private bool _isPlayerOneOverPass = false;
+    private bool _isPlayerTwoUnderPass = false;
+    private bool _isPlayerTwoOverPass = false;
 
     private void OnEnable()
     {
@@ -55,16 +61,56 @@ public class BridgeHandler : MonoBehaviour
         bool isPlayerOne = player.name == "PlayerOne";
         bool isUnderPass = type == BridgeTriggerType.Underpass;
 
+        if (player.TryGetComponent(out CapsuleCollider2D collider))
+        {
+            if (isPlayerOne)
+            {
+                _isPlayerOneUnderPass = isUnderPass;
+                _isPlayerOneOverPass = !isUnderPass;
+                if (_playerOneCollider == null)
+                {
+                    _playerOneCollider = collider;
+                }
+            }
+            else
+            {
+                _isPlayerTwoUnderPass = isUnderPass;
+                _isPlayerTwoOverPass = !isUnderPass;
+                if (_playerTwoCollider == null)
+                {
+                    _playerTwoCollider = collider;
+                }
+            }
+            HandleVehicleCollision();
+            HandleBridgeCollision(isUnderPass, collider);
+        }
+
         if (player.TryGetComponent(out LayerHandler layerHandler))
         {
             layerHandler.SetBridgeSortingLayer(isUnderPass);
         }
-        if (player.TryGetComponent(out CapsuleCollider2D collider))
+    }
+
+    private void HandleBridgeCollision(bool isUnderPass, CapsuleCollider2D collider)
+    {
+        Physics2D.IgnoreCollision(collider, UnderPassInCollider, isUnderPass);
+        Physics2D.IgnoreCollision(collider, UnderPassOutCollider, isUnderPass);
+        Physics2D.IgnoreCollision(collider, OverPassInCollider, !isUnderPass);
+        Physics2D.IgnoreCollision(collider, OverPassOutCollider, !isUnderPass);
+    }
+
+    private void HandleVehicleCollision()
+    {
+        if (_playerOneCollider != null && _playerTwoCollider != null)
         {
-            Physics2D.IgnoreCollision(collider, UnderPassInCollider, isUnderPass);
-            Physics2D.IgnoreCollision(collider, UnderPassOutCollider, isUnderPass);
-            Physics2D.IgnoreCollision(collider, OverPassInCollider, !isUnderPass);
-            Physics2D.IgnoreCollision(collider, OverPassOutCollider, !isUnderPass);
+            if ((_isPlayerOneOverPass && _isPlayerTwoUnderPass) || (_isPlayerOneUnderPass && _isPlayerTwoOverPass))
+            {
+                Physics2D.IgnoreCollision(_playerOneCollider, _playerTwoCollider, true);
+            }
+            else
+            {
+                Physics2D.IgnoreCollision(_playerOneCollider, _playerTwoCollider, false);
+            }
         }
     }
 }
